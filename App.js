79ee026c88e67ee6944ddc6a500f620aa75b3046
WojciechGalecki <wojciech.galecki@wp.pl>
2025-02-13 12:37:53 +0100

    splitting logic across custom components

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
 import { useState } from 'react';
-import { Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
+import { FlatList, StyleSheet, View } from 'react-native';
+
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
-  const [newGoal, setNewGoal] = useState('');
   const [goals, setGoals] = useState([]);
 
-  function goalInputHandler(goal) {
-    setNewGoal(goal);
-  }
-
-  function addGoalHandler() {
+  function addGoalHandler(newGoal) {
     setGoals(prevGoals => [
       ...prevGoals,
       {
@@ -22,23 +20,12 @@ export default function App() {
 
   return (
     <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.textInput}
-          placeholder='Your goal'
-          onChangeText={goalInputHandler}
-        />
-        <Button title='Add Goal' onPress={addGoalHandler} />
-      </View>
+      <GoalInput onAddGoal={addGoalHandler} />
       <View style={styles.goalsContainer}>
         <FlatList
           data={goals}
-          renderItem={(itemData) => {
-            return <View style={styles.goalItem}>
-              <Text style={styles.goalText}>{itemData.item.text}</Text>
-            </View>
-          }}
-          keyExtractor={(item, _index) => item.id }
+          renderItem={(itemData) => <GoalItem text={itemData.item.text} />}
+          keyExtractor={(item, _index) => item.id}
           alwaysBounceVertical={false}
         />
       </View>
@@ -52,32 +39,7 @@ const styles = StyleSheet.create({
     paddingTop: 50,
     paddingHorizontal: 16,
   },
-  inputContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: '#cccccc',
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: '#cccccc',
-    width: '70%',
-    marginRight: 8,
-    padding: 8,
-  },
   goalsContainer: {
     flex: 5,
-  },
-  goalItem: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 6,
-    backgroundColor: '#5e0acc',
-  },
-  goalText: {
-    color: 'white',
   }
 });
